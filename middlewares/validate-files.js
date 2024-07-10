d6af457d493a22005ba4d04dfe574a8623b55b69
Andres Jacinto <Ahndrsx@gmail.com>
2024-07-10 14:33:24 -0600

    Actualizacion 1.2.6 DUMMY 4

--- middlewares/validate-files.js
+++ middlewares/validate-files.js
@@ -2,13 +2,12 @@ const { response } = require("express")
 
 const validateUploads = (req, resp = response, next) => {
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.uploadFiles) {
+        console.log('-----------------------------------------------')
+        // console.log(req.files.uploadFiles)
+        console.log(req)
+        console.log('-----------------------------------------------')
         return resp.status(400).json({msg: "No files were uploaded!!!"});
-      }
-      
-    console.log('-----------------------------------------------')
-    // console.log(req.files.uploadFiles)
-    console.log(req)
-    console.log('-----------------------------------------------')
+      }    
     next()
 }
 
