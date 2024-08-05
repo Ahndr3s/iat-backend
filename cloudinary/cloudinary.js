c8390a414f9d36b93731fa261051eb33c41f22a2
Andres Jacinto <Ahndrsx@gmail.com>
2024-08-05 15:36:34 -0600

    Actualizacion 1.2. patch 2.12

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -46,7 +46,8 @@ require('dotenv').config()
 cloudinary.config({ 
     cloud_name: process.env.CLOUDNAME, 
     api_key: process.env.CLOUDAPIKEY, 
-    api_secret: process.env.CLOUDAPISECRET // Click 'View Credentials' below to copy your API secret
+    api_secret: process.env.CLOUDAPISECRET,
+    upload_preset: process.env.UPLOADPRESET// Click 'View Credentials' below to copy your API secret
 });
 
 module.exports = cloudinary
